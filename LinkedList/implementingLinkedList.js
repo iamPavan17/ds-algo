722d7c73fca4b15dd9824fb7eecc3a47c0be8283
Pavan S <pavan@bambu.co>
2023-12-06 17:08:23 +0530

    ✨ implementing linked-list > append

---
+++ LinkedList/implementingLinkedList.js
@@ -0,0 +1,47 @@
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    // Approch 1 - START
+    //   let currentNext = this.head.next;
+    //   if (!currentNext) {
+    //     this.head.next = newNode;
+    //   }
+
+    //   while (currentNext) {
+    //     if (!currentNext.next) {
+    //       currentNext.next = newNode;
+    //       break;
+    //     } else {
+    //       currentNext = currentNext.next;
+    //     }
+    //   }
+    //   this.tail = newNode;
+    // Approch 1 - END
+
+    // Approch 2 - START
+    this.tail.next = newNode;
+    this.tail = newNode;
+    // Approch 2 - END
+
+    this.length++;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(25);
+console.log(myLinkedList);
