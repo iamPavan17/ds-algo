bc53766a6baf1a2d01978f82b18d8a45f7061f5e
Pavan S <iampavan05@gmail.com>
2023-12-08 16:51:10 +0530

    ✨ implementing stacks

---
+++ Stacks/implementingStacks.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+
+  push(value) {
+    this.data = [...this.data, value]; // or this.data.push(value)
+    return this;
+  }
+
+  pop() {
+    this.data.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+myStack.pop();
+
+console.log(myStack.peek());
+
+console.log(myStack);
