3b740e25c4844e7bd2ce203f0d23a70698445c1c
Pavan S <iampavan05@gmail.com>
2023-12-09 15:49:59 +0530

    ✨ fact > recursion

---
+++ Recursion/factorial.js
@@ -0,0 +1,22 @@
+// Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
+
+function findFactorialRecursive(number) {
+  if (number <= 0) {
+    return 1;
+  }
+
+  return number * findFactorialRecursive(number - 1);
+}
+
+function findFactorialIterative(number) {
+  let answer = 1;
+
+  if (number <= 0) return 1;
+  for (let i = 1; i <= number; i++) {
+    answer *= i;
+  }
+  return answer;
+}
+
+// console.log(findFactorialIterative(2));
+console.log(findFactorialRecursive(5));
