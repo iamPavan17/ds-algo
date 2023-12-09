44eaba72c8e60e752db97714ed5953f53310f38c
Pavan S <iampavan05@gmail.com>
2023-12-09 22:16:01 +0530

    ✨ fib > recursion

---
+++ Recursion/fibonacci.js
@@ -0,0 +1,30 @@
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+function fibonacciIterative(n) {
+  if (n <= 2) return 1;
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 1; i < n; i++) {
+    answer = a + b;
+    a = b;
+    b = answer;
+  }
+
+  return answer;
+}
+console.log(fibonacciIterative(6));
+
+function fibonacciRecursive(n) {
+  if (n <= 1) return n;
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciRecursive(6));
